7ac40d93f19889b704dc053923a05aaf848ca25e
Buildbot <airmontci@airmont.no>
2024-08-19 16:36:45 +0200

    Deleted all, back to start

--- app.js
+++ app.js
@@ -10,7 +10,17 @@ let userInfo = {};
 let questions = [];  // Tom liste som fylles dynamisk
 
 
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAujHYBr458zucLPt_FUQMaZfBustO7LR0",
+    authDomain: "qme1-423d8.firebaseapp.com",
+    projectId: "qme1-423d8",
+    storageBucket: "qme1-423d8.appspot.com",
+    messagingSenderId: "425603580627",
+    appId: "1:425603580627:web:c671dd03b1c40136370a26",
+    measurementId: "G-CSQGSQWCXT"
+    };
+  
   // Initialiser Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
--- public/config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyAujHYBr458zucLPt_FUQMaZfBustO7LR0',
-  authDomain: 'qme1-423d8.firebaseapp.com',
-  projectId: 'qme1-423d8',
-  storageBucket: 'qme1-423d8.appspot.com',
-  messagingSenderId: '425603580627',
-  appId: '1:425603580627:web:c671dd03b1c40136370a26',
-  measurementId: 'G-CSQGSQWCXT'
-};
